3c1307d6ca59ff4a758dc09c441548c900c0bb08
akshayyasawant <akshayya.sawant@gmail.com>
2024-06-21 13:04:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-const con=mysql.createConnection({
-	host:"localhost",
-	user:"root",
-	password:"abc123",
-	database:"fb21june24"
+let con=mysql.createConnection({
+		host:"localhost",
+		user:"root",
+		password:"abc123",
+		database:"ss20june24"
+
 });
 
 app.post("/save",(req,res)=>{
-	let data=[req.body.name,req.body.feedback];
-	let sql="insert into student values(?,?)";
+	let data=[req.body.name,req.body.company,req.body.pkg];
+	console.log(data);
+	let sql="insert into student values(?,?,?)";
 	con.query(sql,data,(err,result)=>{
-		if(err)		res.send(err);
+		if(err) 	res.send(err);
 		else		res.send(result);
-	});
+});
 });
 
 app.listen(9000,()=>{console.log("ready to serve @9000");});
+	
+
+
